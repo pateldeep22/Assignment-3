7346936440f0efc830802969d98030ef4acafbfa
pateldeep22 <deepp1267@gmail.com>
2025-07-12 07:10:29 +0530

    Initial

---
+++ task2.py
@@ -0,0 +1,10 @@
+import math
+from math import *
+
+num=int(input("Enter a number: "))
+square_root=math.sqrt(num)
+print("The square root of ",num," is:",square_root)
+log_base_e=math.log(num)
+print("The logarithm of ",num," is:",log_base_e)
+sine=math.sin(num)
+print("The sine of ",num," is:",sine)
